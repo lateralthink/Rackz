01d57278a280d149d846c9b4717e7eb8b307e3b3
Garito <garito@gmail.com>
2022-05-24 15:43:24 +0200

    Thank you page + some changed (tailwind export what it wants)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   safelist: [
+    'lt-album',
     'lt-album-card-price-and-amount',
     'lt-album-card-price',
     'mlkln3-no-access-passports',
